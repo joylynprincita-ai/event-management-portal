5442b6813a73382c0cd665e82b3fe98cfadc4d0a
joylynprincita-ai <joylynprincita@gmail.com>
2026-07-23 08:27:49 +0530

    Delete Register.jsx

--- Register.jsx
+++
@@ -1,3 +0,0 @@
-export default function Login(){
-return <div style={{padding:40}}><h2>Login</h2><input placeholder="Email"/><br/><br/><input type="password" placeholder="Password"/><br/><br/><button>Login</button></div>
-}
