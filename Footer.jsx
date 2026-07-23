d43ff22dfafde60156cd095d56901c5b106c9ac1
joylynprincita-ai <joylynprincita@gmail.com>
2026-07-23 08:25:51 +0530

    Delete Footer.jsx

--- Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer style={{padding:20,textAlign:"center",background:"#111827",color:"#fff",marginTop:40}}>
-      © 2026 Event Management Portal
-    </footer>
-  );
-}
